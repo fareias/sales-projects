9bcc6ed53669ea487ad77604929ee2ec35f5a7e4
Fernando Areias <fareias@gmail.com>
2015-05-07 10:12:23 -0300

    Quotation applied to calculations

--- simulator.js
+++ simulator.js
@@ -1,15 +1,41 @@
 (function() {
-	var form = document.getElementById('myform'),
-		resources = 0,
+
+// Jsonp function to get current quotations	
+
+	var quote = function (from, to, callback) {
+		var conversion = (from + '_' + to).toUpperCase();
+		var uid = 'jsonp_' + Date.now();
+		window[uid] = function(data) {
+			callback(data[conversion].val);
+		};
+		var transport = document.createElement('script');
+		transport.src = 'http://www.freecurrencyconverterapi.com/api/v3/convert?q=' + conversion + '&compact=y&callback=' + uid;
+		transport.onload = function() {
+			transport.parentNode.removeChild(transport);
+			delete window[uid];
+		};
+		document.head.appendChild(transport);
+	};
+
+// Variables definitions
+
+	var	resources = 0,
 		client = '',
 		duration= 0,
 		otherCosts = 0,
 		margin = 0,
 		manHour = 0,
 		finalPrice = 0,
-		finalCost= 0;
+		finalCost= 0,
+		averageCost = 90,
+		hiringTaxes = 0.76,
+		incomeTaxes = 0.18,
+		currency = document.getElementById('currency'),
+		form = document.getElementById('myform');
 
-	form.addEventListener('input', function(event) {
+// Function to get inputs, work with them and give Simulator outputs depending on quotation value
+
+	var listener = function(event) {
 		var id = event.target.id,
 			value = event.target.value;
 
@@ -28,26 +54,44 @@
 				break;
 			case 'othercosts':
 				otherCosts = parseFloat(value);
-				document.getElementById('otherInput').innerHTML = "R$" + otherCosts;
+				document.getElementById('otherInput').innerHTML = otherCosts.toFixed(2);
 				break;
 			case 'margin':
 				margin = parseFloat(value);
 				document.getElementById('marginInput').innerHTML = margin + "%";
 				break;
-		}
+		};
+
+// Calculations
+
+		if (currency.options[currency.selectedIndex].text === "BRL") {
+
+			finalCost = (duration * averageCost * (1 + hiringTaxes)) + otherCosts;
+				document.getElementById('finalCost').innerHTML = "R$" + finalCost.toFixed(2);
+				
+			finalPrice = finalCost/((1 - incomeTaxes) - (margin/100));
+				document.getElementById('finalPrice').innerHTML = "R$" + finalPrice.toFixed(2);
+
+		} else {
+			
+			quote('usd', 'brl', function(data) {
 		
-	var averageCost = 90;
-	var hiringTaxes = 0.76;
-	var incomeTaxes = 0.18;
+				document.getElementById('quotation').innerHTML = "Quotation:" + " " + data;
 
-	finalCost = (duration * averageCost * (1 + hiringTaxes)) + otherCosts;
-		document.getElementById('finalCost').innerHTML = "R$" + finalCost.toFixed(2);
-		
+				finalCost = (duration * averageCost * (1 + hiringTaxes))/data + otherCosts;
+					document.getElementById('finalCost').innerHTML = "USD" + finalCost.toFixed(2);
+					
+				finalPrice = finalCost/((1 - incomeTaxes) - (margin/100));
+					document.getElementById('finalPrice').innerHTML = "USD" + finalPrice.toFixed(2);
+				
+				});			
+			};	
+	};
 
-	finalPrice = finalCost/((1 - incomeTaxes) - (margin/100));
-		document.getElementById('finalPrice').innerHTML = "R$" + finalPrice.toFixed(2);
+// Adding event listeners
 
-	});
-
-
+	form.addEventListener('input', listener);
+	currency.addEventListener('change', listener);
+	
 })();
+
